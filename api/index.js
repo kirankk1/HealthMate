8efca222618afa884e98057cc5188f3f33289205
= <porlakiran@gmail.com>
2024-10-21 18:07:53 +0530

    #

--- api/index.js
+++ api/index.js
@@ -5,9 +5,8 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import medicineRoutes from "./routes/medicineRoutes.js";
 import myMedicineRoutes from "./routes/myMedicineRoute.js";
-import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from 'path'
+import path from 'path';
 
 dotenv.config();
 
@@ -15,21 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const __dirname = path.resolve()
-
-// CORS configuration
-const corsOptions = {
-  origin: ['http://localhost:5173',
-    'https://healthmate-zrlf.onrender.com'],
-
-    //frontend url
-  credentials: true, // Allow credentials
-};
-
-// Use CORS middleware
-app.use(cors(corsOptions));
-
-
+const __dirname = path.resolve();
 
 mongoose
   .connect(process.env.MONGO)
@@ -43,13 +28,17 @@ mongoose
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/medicines", medicineRoutes);
-app.use("/api/myMedicines", myMedicineRoutes); 
+app.use("/api/myMedicines", myMedicineRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
+// Serve frontend files
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+// Handle any requests that don't match the above routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -60,6 +49,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
